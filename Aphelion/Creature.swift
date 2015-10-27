d807f477a0f5303b812c0a18633c159514eb2017
cscalcucci <cscal@aphelion-enterprises.com>
2015-10-27 19:44:31 -0400

    First Commit

---
+++ Aphelion/Creature.swift
@@ -0,0 +1,9 @@
+//
+//  Creature.swift
+//  Aphelion
+//
+//  Created by Christopher Scalcucci on 10/25/15.
+//  Copyright © 2015 Aphelion. All rights reserved.
+//
+
+import Foundation
---
+++ Aphelion/Utility.swift
@@ -0,0 +1,9 @@
+//
+//  Utility.swift
+//  Aphelion
+//
+//  Created by Christopher Scalcucci on 10/25/15.
+//  Copyright © 2015 Aphelion. All rights reserved.
+//
+
+import Foundation
